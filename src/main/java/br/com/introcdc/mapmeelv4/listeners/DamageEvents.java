50d7a617b49a02b99fa9efb2193c6ab308ea9eb4
Intro <introgamerhd@live.com>
2017-08-25 07:39:22 -0300

    Sponge Events

---
+++ src/main/java/br/com/introcdc/mapmeelv4/listeners/DamageEvents.java
@@ -0,0 +1,30 @@
+package br.com.introcdc.mapmeelv4.listeners;
+/*
+ * Writter by IntroCDC, Bruno Coêlho at 25/08/2017 - 07:35
+ */
+
+import br.com.introcdc.mapmeelv4.MapUtils;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class DamageEvents extends MapUtils implements Listener {
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            if (event.getCause().equals(EntityDamageEvent.DamageCause.FALL)) {
+                if (MoveEvent.spongeDamage.contains(event.getEntity().getUniqueId())) {
+                    event.setCancelled(true);
+                }
+                if (event.getEntity().getLocation().clone().add(0, -1, 0).getBlock().getType().equals(Material.SPONGE)) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+
+    }
+
+}
--- src/main/java/br/com/introcdc/mapmeelv4/listeners/MoveEvent.java
+++ src/main/java/br/com/introcdc/mapmeelv4/listeners/MoveEvent.java
@@ -5,24 +5,39 @@ package br.com.introcdc.mapmeelv4.listeners;
 
 import br.com.introcdc.mapmeelv4.level.Level;
 import br.com.introcdc.mapmeelv4.level.LevelObjective;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class MoveEvent implements Listener {
 
+    public static List<UUID> spongeDamage = new ArrayList<>();
+    public static Vector vector = new Vector(0, 2, 0);
+
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
-        if(event.getTo().getWorld().getName().equalsIgnoreCase("world")) {
-            for(Level level : Level.getLeveis().values()) {
-                if(event.getTo().getBlock().getType().equals(level.getBlockId().getMaterial()) && event.getTo().getBlock().getData() == level.getBlockId().getData()) {
+        if (event.getTo().clone().add(0, -1, -0).getBlock().getType().equals(Material.SPONGE)) {
+            if (!spongeDamage.contains(event.getPlayer().getUniqueId())) {
+                spongeDamage.add(event.getPlayer().getUniqueId());
+            }
+            event.getPlayer().setVelocity(vector);
+        }
+        if (event.getTo().getWorld().getName().equalsIgnoreCase("world")) {
+            for (Level level : Level.getLeveis().values()) {
+                if (event.getTo().getBlock().getType().equals(level.getBlockId().getMaterial()) && event.getTo().getBlock().getData() == level.getBlockId().getData()) {
                     level.joinPortal(event.getPlayer());
                     Level.currentLevel = level;
                 }
             }
-        } else if(Level.currentLevel != null){
-            for(LevelObjective levelObjective : Level.currentLevel.getObjectives()) {
-                if(event.getTo().getBlock().getType().equals(levelObjective.getBlockId().getMaterial()) && event.getTo().getBlock().getData() == levelObjective.getBlockId().getData()) {
+        } else if (Level.currentLevel != null) {
+            for (LevelObjective levelObjective : Level.currentLevel.getObjectives()) {
+                if (event.getTo().getBlock().getType().equals(levelObjective.getBlockId().getMaterial()) && event.getTo().getBlock().getData() == levelObjective.getBlockId().getData()) {
                     Level.currentLevel.unloadLevel();
                 }
             }
